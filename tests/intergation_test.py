b64e6b255377319364fe06738c0782ca95582f99
Abdelhak <ab.marouane@gmail.com>
2017-06-06 11:13:35 -0500

    hsjhs

---
+++ tests/intergation_test.py
@@ -0,0 +1,16 @@
+import unittest
+import requests
+
+class TestpyGHRCCatalogueModules(unittest.TestCase):
+    testESIndex="test_index"
+    testEStype="test_type"
+    testDatasetId="dummyEntry"
+
+
+    def test_ghrc_configFile(self):
+        
+        self.assertTrue(1==1)
+    def test_number_2(self):
+        A=1
+        B=3
+        self.assertEqual(A+B,4)
